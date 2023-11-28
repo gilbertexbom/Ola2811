1b5e20efca918ff5cfe56f27257d48ac67dc58b6
Instrutor <gilberto.braga0021@gmail.com>
2023-11-28 09:30:58 -0300

    - Commit do tio.

---
+++ Ola.py
@@ -0,0 +1,10 @@
+# Programa que diz bom dia a o usuário
+
+# Apresetação
+print('\n\t\t\t -- Dia Bom Dia ao Usuário -- \n')
+
+# Entrada
+nome = input('Informe o seu nome: ')
+
+# Saída
+print('Bom dia ' + nome + '.')
